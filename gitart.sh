a2ff33d039aef2e06f78607ac6739abdacc8df6b
Mikhail Bulash <scaryspiderpig@gmail.com>
2014-04-14 01:18:12 +0300

    Some cleanup

--- gitart.sh
+++ gitart.sh
@@ -21,24 +21,18 @@ email="${input[9]}"
 repo="${input[10]}"
 startdate="${input[11]}"
 
+height=7
+width=$(($(head -$height "$1" | wc -L)-1))
+
 
 # Repo configuration
 mkdir "$repo"
 cd "$repo"
+
 git init -q
 git config user.name "$username"
 git config user.email "$email"
 git remote add origin "https://github.com/$username/$repo.git"
-
-# Input preparation
-max=0
-for (( i = 0; i < 7; i++ )); do
-	if [ "${#input[i]}" -gt "$max" ]; then
-		max="${#input[i]}"
-	fi
-done
-width=$(($max-1))
-height=7
 
 
 # Action!
@@ -46,21 +40,21 @@ height=7
 # Save current date
 date +%s > ../saved_date
 
-date="$(date +%s -d "$startdate")"
-date=$(($date + 43200))
-date -s @"${date}" > /dev/null
+fakedate=$(date +%s -d "$startdate")
+fakedate=$(($fakedate + 43200))
+date -s @$fakedate > /dev/null
 touch file
 
 for (( i = 0; i < width; i++ )); do
 	for (( j = 0; j < height; j++ )); do
 		char="${input[j]:i:1}"
-		if [ -n "${char}" ] && [ "${char}" != ' ' ]; then
+		if [ -n "$char" ] && [ "$char" != ' ' ]; then
 			echo . >> file
 			git add file
 			git commit -m "." -q
 		fi
-		date=$(($date + 86400))
-		date -s @"${date}" > /dev/null
+		fakedate=$(($fakedate + 86400))
+		date -s @$fakedate > /dev/null
 	done
 done
 
